daf0f40255adce055dfa599ff508fce22158055b
danyel-dev <dannykill4221x@gmail.com>
2023-10-19 14:35:52 -0300

    core app created and List, Item models created

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class List(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    
+    
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    List = models.ForeignKey(List, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    done = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.name
